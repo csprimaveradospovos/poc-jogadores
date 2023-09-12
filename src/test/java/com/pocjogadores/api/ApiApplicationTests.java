983c6ee549aa2a69967b6b844fe60b8e0d7d1e4a
guineitor <guineitor.traps236@gmail.com>
2023-09-11 21:24:37 -0300

    projeto base inicializado

---
+++ src/test/java/com/pocjogadores/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocjogadores.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
